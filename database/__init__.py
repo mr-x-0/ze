dc978033b8e36ba7ec3d45ce4e30545c61ebc09b
mr-x-0 <urfavmody@gmail.com>
2025-01-30 01:22:25 +0200

    Add files via upload

---
+++ database/__init__.py
@@ -0,0 +1,17 @@
+from .core.getdb import pyDatabase
+
+#to import database 
+zedB = zedB = pyDatabase()
+
+
+#for old 
+gvarstatus = zedB.get_key
+addgvar = zedB.set_key
+delgvar = zedB.del_key 
+
+#for something to store it 
+
+InlinePlugin = {}
+InlinePaths = []
+CMD_HELP = {}
+LIST = {}
