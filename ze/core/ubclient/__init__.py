461768d49bc4e40bc9efe5cc8f7d34764476d958
mr-x-0 <urfavmody@gmail.com>
2025-01-30 16:02:23 +0200

    Update __init__.py

--- ze/core/ubclient/__init__.py
+++ ze/core/ubclient/__init__.py
@@ -1,2 +1,16 @@
+import os, sys
+os.system("pip install telethon")
+os.system("pip install requests")
+os.system("pip install ipaddress")
+os.system("pip install tabulate")
+os.system("pip install redis")
+os.system("pip install telegraph")
+os.system("pip install python-decouple")
+os.system("pip install pytz")
+os.system("pip install Pillow")
+os.system("pip install yt-dlp")
+os.system("pip install gitpython")
+os.system("pip install ytdl")
+os.system("pip install youtube-search-python")
 from .client import *
 from . import inlinebuilder, conversation
